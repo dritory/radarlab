e51acf2f9edc933fa88711e764060fbe2d03ed52
Endre Dåvøy <lolskap@live.no>
2020-04-02 14:42:47 +0200

    added lots of data and finished analysis

--- data.py
+++ data.py
@@ -8,10 +8,22 @@ from scipy import signal as sgn
 #v=0,26 for data 31-35
 #v=0,69 for data 24-28
 
-roiDict = {31:(170000,192000),32:(163800,170000),33:(140000,180000),34:(130000,150000),
+roiDict = {
+        1:(70000,190000),2:(40000,190000),3:(90000,190000),4:(60000,150000),
+        5:(90000,140000),6:(60000,130000),7:(40000,100000),8:(110000,240000),
+        9:(90000,120000),10:(90000,130000),
+
+
+        31:(170000,192000),32:(163800,170000),33:(140000,180000),34:(130000,150000),
            41:(30000,42700),42:(30000,45000),43:(70000,80000),44:(40000,50000),
            51:(58700,80000),52:(60000,70800),53:(70000,80000),54:(60000,70000),
            61:(50000,60000),62:(60000,70000),63:(60000,70000),64:(60000,70000)
+}
+
+velocities = {
+    1:0.53,2:0.71,3:0.88,4:1.04,5:0.45,6:1.23,7:1.66,8:0.77,9:0.32,10:1.35,
+    21:0.76,22:0.83,23:0.77,24:0.85,25:0.89,26:1.92,27:1.83,28:1.85,29:0.54
+    
 }
 
 
@@ -19,15 +31,16 @@ sample_period = 32
 
 def prepros (data, maximum = 4096):
     d = sgn.detrend(data,axis=0)
-
+    ba = sgn.butter(2, [0.0016], btype='high')
+    d = sgn.lfilter(ba[0], ba[1], d, axis=0)
     return d
     
         
 
 def notch_filter (data):
-    b1 = [0.99,-1.831,0.99,]
-    a1 = [1,-1.831,0.9801]
-    data = sgn.lfilter(b1,a1, data, axis=0)
+    #b1 = [0.99,-1.831,0.99,]
+    #a1 = [1,-1.831,0.9801]
+    #data = sgn.lfilter(b1,a1, data, axis=0)
 
     #ba = sgn.butter(3,[0.01312*2, 0.01440*2],btype='bandstop')
     #data = sgn.lfilter(ba[0],ba[1], data, axis=0)
@@ -35,30 +48,57 @@ def notch_filter (data):
     return data
 
 
-def plotData(data):
+def plotDatas(data):
     for d in data:
         plt.title("Measurement " + str(d[1]) + " Velocity " + str(d[2]) + " m/s")
         plt.plot(d[0][:,1])
         plt.show()
-def plotFFTData(data):
+
+def plotData(data):
+    d = data
+    plt.title("Measurement " + str(d[1]) + " Velocity " + str(d[2]) + " m/s")
+    plt.plot(d[0][:,1])
+    plt.show()
+def plotFFTDatas(data):
     f_0 = 24.13e9
     c = 3e8
     for d in data:
         
         plt.title("Measurement " + str(d[1]) + " Velocity " + str(d[2]) + " m/s")
-        f = d[2]*((4*f_0))/(c)
-        plt.axvline(x=f, ymin=0, ymax=50, label='Doppler frequency')
+        f = d[2]*((2*f_0))/(c)
+        plt.axvline(x=f, ymin=0, ymax=50, label='Doppler frequency', color='g')
         fft = (np.abs(np.fft.fft(d[0][:,1])[:-2]))
         plt.xlim(-3000,3000)
         plt.plot(np.fft.fftshift(np.fft.fftfreq(len(fft),sample_period/(1e6))),np.fft.fftshift(fft))
         plt.show()
+def plotFFTData(data):
+    f_0 = 24.13e9
+    c = 3e8
+    d = data
+    plt.title("Measurement " + str(d[1]) + " Velocity " + str(d[2]) + " m/s")
+    f = d[2]*((2*f_0))/(c)
+    plt.axvline(x=f, ymin=0, ymax=50, label='Doppler frequency', color='g')
+    fft = (np.abs(np.fft.fft(d[0][:,0])[:-2]))
+    plt.xlim(-3000,3000)
+    plt.plot(np.fft.fftshift(np.fft.fftfreq(len(fft),sample_period/(1e6))),np.fft.fftshift(fft))
+    plt.show()
 
-def getRadarDatas(start, end, velocity):
+def getRadarDatas(start, end):
     datas = [0]*(end - start)
     for i in range(start,end):
-        s = roiDict[i][0]
-        e = roiDict[i][1]
-        data1 = (raspi_import("./out/radarData" + str(i) + ".bin"))[1][s:e,3:5]
+        try:
+            s = roiDict[i][0]
+            e = roiDict[i][1]
+        except KeyError:
+            print("Roi not found")
+            s = 0
+            e = -1
+        try:
+            velocity = velocities[i]
+        except KeyError:
+            raise "Velocity not found"
+
+        data1 = (raspi_import("./out/radar" + str(i) + ".bin"))[1][s:e,3:5]
         data1 = prepros(data1)
         data1 = notch_filter(data1)
         #data1 = filter(data1)
@@ -67,29 +107,44 @@ def getRadarDatas(start, end, velocity):
         datas[i - start] = [data1, i, velocity]
     return datas
 
-def getRadarData(num, velocity):
-    s = roiDict[i][0]
-    e = roiDict[i][1]
-    data1 = (raspi_import("./out/radarData" + str(i) + ".bin"))[1][s:e,3:5]
+def getRadarData(num):
+    s = 0
+    e = -1
+    try:
+        s = roiDict[num][0]
+        e = roiDict[num][1]
+    except KeyError:
+        print("Roi not found")
+        s = 0
+        e = -1
+    velocity = 0
+    try:
+        velocity = velocities[num]
+    except KeyError:
+        raise Exception("Velocity not found")
+    
+    
+    data1 = (raspi_import("./out/radar" + str(num) + ".bin"))[1][s:e,3:5]
     data1 = prepros(data1)
     #data1 = notch_filter(data1)
     #data1 = filter(data1)
-    #print(data1)
 
-    return [data1, i, velocity]
+    return [data1, num, velocity]
     
 
 
-#data1 = [getRadarDatas(24,28), 0.69]
-data2 = getRadarData(1, 0.53)
-data3 = getRadarData(2, 0.71)
-data4 = getRadarData(3, 0.88)
-data5 = getRadarData(4, 1.04)
+if __name__ == "__main__":
 
-#plotData(data2[0])
-#plotData(data3)
-plotFFTData(data3)
-#plotData(data4)
-#plotData(data5)
+    #data1 = [getRadarDatas(24,28), 0.69]
+    data2 = getRadarData(1)
+    data3 = getRadarData(2)
+    data4 = getRadarData(3)
+    data5 = getRadarData(4)
 
-#print(getRadarDatas(31,35))
+    #plotData(data2[0])
+    #plotData(data3)
+    plotFFTDatas(getRadarDatas(21,30))
+    #plotData(data4)
+    #plotData(data5)
+
+    #print(getRadarDatas(31,35))
--- estimate_velocity.py
+++ estimate_velocity.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import scipy.signal as sgn
 
 from raspi_import import raspi_import
-
 import data
 
 def prepros (data, maximum = 4096):
+    #normalize to 0 - 1
     d = data / maximum
-    #d = d - 0.5
     try:
-        d = sgn.detrend(d)
+        #remove the average, so that the signal goes from -1 to 1
+        d = sgn.detrend(d) 
     except ZeroDivisionError:
         d = d - np.mean(d)
     #d = d - np.mean(d)
@@ -30,31 +30,44 @@ def findCorrPeaks(data, plot=False):
     data = sgn.resample(data, len(data)*resample_coeff)
     
     maxlag = min(1000, len(data))
-    number_of_peaks = 10
+    number_of_peaks = 5
     corr = windowedcorr(data,data,maxlag)
+    
+    #corr = sgn.hilbert(np.abs(corr), len(corr)//2)
 
     abs_corr = np.abs(corr)[((len(corr)//2)):len(corr)]
-    
-    max_peaks = sgn.find_peaks(abs_corr, prominence=0.05)[0]
+    fft = np.abs(np.fft.fft(abs_corr))[0:maxlag//2]
+    fft[0] = 0 #remove 0 hz
+
+    max_fft_peak = np.where((fft == np.amax(fft)))[0][0]
+
+    max_peak = 1000 / max_fft_peak
+
+    max_peaks = sgn.find_peaks(abs_corr, prominence=0.1)[0]
 
     max_peaks = max_peaks[0: min(number_of_peaks, len(max_peaks) ) ]
     if(plot):
+        print(max_peaks)
+        print(max_fft_peak, max_peak)
         plt.plot(abs_corr, "-d", markevery=max_peaks.tolist())
         plt.show()
+        plt.plot(fft)
+        plt.show()
     
-    return max_peaks/resample_coeff
+    return max_peaks/resample_coeff, max_peak/resample_coeff
 
 
-def findVelocity(data, start= 0, stop = -1):
-    q = prepros(data[start:stop,0])
-    i =  prepros(data[start:stop,1])
+def findVelocity(data, plot=False):
+
+    q = prepros(data[0][:,0])
+    i =  prepros(data[0][:,1])
 
     fft_i = np.abs(np.fft.fft(i)[:len(i)//2])
     fft_q = np.abs(np.fft.fft(q)[:len(i)//2])
     
     direction = 1 if ( np.mean(np.arctan2(fft_q, fft_i)) > 0 ) else -1
 
-    peaks  = findCorrPeaks(i)
+    peaks, peak  = findCorrPeaks(i, plot)
     if(len(peaks) > 0):
         arr = np.zeros(len(peaks) - 1)
         for n in range(len(peaks)-1):
@@ -68,6 +81,10 @@ def findVelocity(data, start= 0, stop = -1):
     else:
         v_corr = 0
         print("Could not compute velocity by correlation")
+
+    v_corr_fft = ( c*(31250/(peak*2)) )/ (2*(f_0))
+
+
     m = np.amax(fft_i)
     i = np.where(fft_i == m)[0][0]
     
@@ -76,29 +93,19 @@ def findVelocity(data, start= 0, stop = -1):
     f_0 = 24.13e9
     c = 3e8
     v = ( c*(fn) )/ (2*(f_0))
+    print("Measurement ", data[1], " Actual velocity ", data[2])
+    print("Estimated velocity: ", v,",", v_corr, ",", v_corr_fft)
+    print("Error ", v-data[2], ",", v_corr-data[2], ",", v_corr_fft-data[2])
 
-    return direction*v, direction*v_corr
+    return str(direction*v) + "," + str(direction*v_corr) + "," + str(direction*v_corr_fft)
 
+def findVelocities(data):
+    vs = [0]*len(data)
+    for i in range(0, len(data)):
+        vs[i] = findVelocity(data[i])
+    return vs
 
-data2 = data.data2
-print (findVelocity(data2[0][0]))
+if __name__ == "__main__":
+    for i in findVelocities(data.getRadarDatas(21,30)):
+        print(i)
 
-
-if(False):
-    sample_period, data1 = raspi_import("./raw_data/adcData_1.bin")
-    sample_period, data2 = raspi_import("./raw_data/adcData_2.bin")
-    sample_period, data3 = raspi_import("./raw_data/adcData_3.bin")
-    sample_period, data4 = raspi_import("./radarData/radarData31.bin")
-
-    data = data4
-    win_len = 10000
-    length = data.shape[0]//win_len
-    v1 = np.zeros(length)
-    v2 = np.zeros(length)
-    num = 200
-    for i in np.linspace(0,length - 1,num):
-        #print(i*win_len, (i + 1)*win_len)
-        v1[int(i)], v2[int(i)] = findVelocity(data, int(i*win_len), int((i + 1)*win_len))
-    plt.plot(v1)
-    plt.plot(v2)
-    plt.show()
